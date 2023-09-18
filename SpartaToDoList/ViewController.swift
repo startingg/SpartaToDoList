0bffda20237aa31343ff5624c43c0a9b29766e88
이시영 <jltldud@gmail.com>
2023-09-18 23:57:58 +0900

    Initial Commit

---
+++ SpartaToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpartaToDoList
+//
+//  Created by t2023-m0064 on 2023/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
